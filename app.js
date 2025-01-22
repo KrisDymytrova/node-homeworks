9e8e7f686204fd02182590300e6165cdfda8be60
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2025-01-22 21:10:58 +0200

    HW 22 Completed

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error("Помилка читання файлу:", err);
-        return;
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`Метод: ${req.method}, URL: ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Привіт з Express.js!');
+});
+
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Користувач з ID: ${userId}`);
+});
+
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).send('Будь ласка, надайте ім\'я та електронну пошту.');
     }
-    console.log("Вміст файлу data.txt:", data);
+
+    res.send(`Отримано дані: Ім'я - ${name}, Електронна пошта - ${email}`);
+});
+
+app.use((req, res) => {
+    res.status(404).send('Сторінка не знайдена');
+});
+
+app.listen(3000, () => {
+    console.log('Сервер працює на порту 3000');
 });
